477ecdd35f3e133695158d9ad9fd119970228dcd
M. Malik Maulana <mmalikmaulana.7@gmail.com>
2026-06-09 22:02:32 +0700

    Menambah SemanticController

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GeminiController;
+use App\Http\Controllers\SemanticController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/tes-ai', [GeminiController::class, 'testKoneksi']);
+Route::get('/api/cari-jurnal', [SemanticController::class, 'cariJurnal']);
