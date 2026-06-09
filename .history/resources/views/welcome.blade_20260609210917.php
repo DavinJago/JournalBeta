58382780265b63b1aa84e283437039dec88511bd
Hamzah Arijal Rahmat <hamzahrahmat188@gmail.com>
2026-06-09 21:19:30 +0700

    menambahkan halaman utama

--- .history/resources/views/welcome.blade_20260609210917.php
+++ .history/resources/views/welcome.blade_20260609210917.php
@@ -5,10 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JurnaLens</title>
 
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet">
 </head>
 <body>
