c2dea95e7d2a855697af95e5e225fe599c259dd7
unknown <darikur7@gmail.com>
2026-06-09 21:46:34 +0700

    updating login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,18 +8,27 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
-    <link href=\css\login.css rel="stylesheet">
-        </head>
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+</head>
 
 <body>
-    <h1>Halo Jurnalers!</h1>
-    <p>Welcome Back!</p>
-    <p>Belum Punya Akun? Register Dulu</p>
-    <form>
-        <p><input type="email" placeholder="Jurnal@gmail.com"></p>
-        <p><input type="password" placeholder="Jurnal"></p>
-        <button>Login Now</button>
-    </form>
+    <div class="formulir">
+        <div>
+            <h1>Halo Jurnalers!</h1>
+            <p>Solusi untukmu Meringkas Jurnal!</p>
+        </div>
+
+        <p>Welcome Back!</p>
+        <p>Belum Punya Akun? Register Dulu</p>
+
+        <form>
+            <div>
+                <p><input type="email" placeholder="Jurnal@gmail.com"></p>
+                <p><input type="password" placeholder="Jurnal"></p>
+                <button type="submit">Login Now</button>
+            </div>
+        </form>
+    </div>
 </body>
 
 </html>
